dac899e36ae3aad690b4c903c7038dc9a9030d1e
dong huang <huangdgm@gmail.com>
2017-11-29 12:26:46 +1300

    rename the method of getYelpData to getYelpRestaurantData

--- controller/RestaurantCard.js
+++ controller/RestaurantCard.js
@@ -5,7 +5,7 @@ var builder = require('botbuilder');
 exports.displayRestaurantCards = function getRestaurantData(foodName, location, session){
     var url ='https://api.yelp.com/v3/businesses/search?term='+foodName+'&location='+location + '&limit=5';
     var auth ='cO92idzWqWjpOsV8RdAoB2DZl2GW8OE8pvoTlOjNNI0gbA2J7xXuiAPtLAYCkPCKR-dIXG3ePsSI4ngt8WRNQ4q4RlKMdXyvJr6r4_L3kndI5wpznLN6WUrPmgDYWXYx';
-    rest.getYelpData(url,auth,session,displayRestaurantCards);
+    rest.getYelpRestaurantData(url,auth,session,displayRestaurantCards);
 }
 
 function displayRestaurantCards(message, session) {
