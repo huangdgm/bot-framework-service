3a360f77c8d676fd3b7727bb7a2d1572e70f7b9c
dong huang <huangdgm@gmail.com>
2017-11-29 20:33:52 +1300

    delete the BotCard.js and move the displayBotCard logic to the LuisDialog.js

--- controller/LuisDialog.js
+++ controller/LuisDialog.js
@@ -148,7 +148,25 @@ exports.startDialog = function (bot) {
 	
 	bot.dialog('WhoAreYou', function (session, args) {
 		if (!isAttachment(session)) {
-			bot.displayBotCard(session);
+			//bot.displayBotCard(session);
+			var attachment = [];
+    
+			// Create a herocard with name, images and hyperlink.
+			var card = new builder.HeroCard(session)
+				.title("I'm the Chat Bot from Contoso bank.")
+				.subtitle("Based on Microsoft bot framework service.")
+				.images([
+					builder.CardImage.create(session, "http://botmywebapp.azurewebsites.net/bot.png")])
+				.buttons([
+					builder.CardAction.openUrl(session, "https://dev.botframework.com/", 'About Me')
+				]);
+			attachment.push(card);
+
+			//Displays bot hero card carousel in chat box 
+			var message = new builder.Message(session)
+				.attachmentLayout(builder.AttachmentLayout.carousel)
+				.attachments(attachment);
+			session.send(message);
 		}
     }).triggerAction({
         matches: 'WhoAreYou'
